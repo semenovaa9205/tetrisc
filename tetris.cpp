5d0f7b9cf720049127e47a1654af3104574a4771
Darksaid <74002782+semenovaa9205@users.noreply.github.com>
2022-06-09 10:29:43 +0300

    relice update

--- tetris.cpp
+++ tetris.cpp
@@ -31,27 +31,38 @@ struct Point
 	int x, y;
 }a[4], b[4];
 int main() {
-	RenderWindow window(VideoMode(N * w, M * w), "Tettris");
+	RenderWindow window(VideoMode(N * w, M * w), "Tetris");
 	Texture t;
-	 t.loadFromFile("C:/Users/andrey/src/tetrisc/Paint/tetris.png");
+	t.loadFromFile("C:/Users/andrey/src/tetrisc/Paint/tetris.png");
+	//t.loadFromFile("C:\\Users\\Alexander\\source\\repos\\tetrisc\\Paint\\tetris.png");
 	//t.loadFromFile("F:\\WorkF\\tmp\\redsquare400x400.png");	
 	Sprite tet(t);
 	tet.setTextureRect(IntRect(0, 0, w, w));
 	int dx = 0;
+	int dy = 0;
+	int fieldWidth = N - 1;
+	int fieldHeight = M - 1;
 	bool rotate = false;
 
-	int n = 3;
-	if(a[0].x==0)
-	for (int i = 0; i < 4; i++)
-	{
-		a[i].x = figures[n][i] % 2;
-		a[i].y = figures[n][i] / 2;
-	}
-	dx = 0;
-	rotate = false;
+
+	Clock timer; 
+	bool redrawScreen = true;
+	bool newFigure = true;
 
 	while (window.isOpen())
 	{
+		if (newFigure)
+		{
+			newFigure = false;
+			int n = (int)(((double)rand() / (double)RAND_MAX) * 6.0 + 0.5);
+			for (int i = 0; i < 4; i++)
+			{
+				a[i].x = figures[n][i] % 2 + fieldWidth / 2 - 1;
+				a[i].y = figures[n][i] / 2;
+			}
+			tet.setTextureRect(IntRect(0 + n*w, 0, w, w));
+		}
+
 		Event event;
 		while (window.pollEvent(event))
 		{
@@ -72,8 +83,73 @@ int main() {
 				{
 					dx = -1;
 				}
+				else if (event.key.code == Keyboard::Down)
+				{
+					dy = 1;
+				}
 			}
+		}
+		if (rotate) {
+			rotate = false;
+			Point p = a[1];
+			for (int i = 0; i < 4; i++)
+			{
+				int x = a[i].y - p.y;
+				int y = a[i].x - p.x;
 
+				a[i].x = p.x - x;
+				a[i].y = p.y + y;
+			}
+			for (int i = 0; i < 4; i++)
+			{
+				if (a[i].x < 0) dx = -a[i].x;
+				if (a[i].x > fieldWidth) dx = -(a[i].x - fieldWidth);
+			}
+			redrawScreen = true;
+		}
+		for (int i = 0; i < 4; i++)
+		{
+			if (a[i].x <= 0 && dx < 0) dx = 0;
+			if (a[i].x >= fieldWidth && dx > 0) dx = 0;
+		}
+
+		if (dx != 0)
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				a[i].x += dx;
+			}
+			redrawScreen = true;
+			dx = 0;
+		}
+
+		if (dy != 0)
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				a[i].y += dy;
+				if (a[i].y >= fieldHeight)
+					newFigure = true;
+			}
+			redrawScreen = true;
+			dy = 0;
+		}
+
+		if (timer.getElapsedTime().asMilliseconds() > 1000)
+		{
+			timer.restart();
+			for (int i = 0; i < 4; i++)
+			{
+				a[i].y++;
+				if (a[i].y >= fieldHeight) 
+					newFigure = true;
+			}
+			redrawScreen = true;
+		}
+
+		if (redrawScreen)
+		{
+			redrawScreen = false;
 			window.clear(Color::White);
 			for (int i = 0; i < 4; i++)
 			{
@@ -81,36 +157,7 @@ int main() {
 				window.draw(tet);
 			}
 			window.display();
-
-		} 
-		if (rotate) {
-			Point p = a[1];
-			for (int i = 0; i < 3; i++)
-			{
-				int x = a[i].y - p.y;
-				int y = a[i].x - p.y;
-
-				a[i].x = p.x - x;
-				a[i].y = p.y + y;
-			}
-		for (int i = 0; i < 4; i++)
-		{
-			if (a[i].x <= 0 && dx < 0) dx = 0;
-			if (a[i].x >= 7 && dx > 0) dx = 0;
 		}
-
-		for (int i = 0; i < 4; i++)
-		{
-			a[i].x += dx;
-		}
-
-		dx = 0;
-
-		
-		}
-
-
-
 	}
 	return 0;
 }
